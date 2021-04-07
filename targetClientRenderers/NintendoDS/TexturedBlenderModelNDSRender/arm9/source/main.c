bd066850ba2bdee4a27a66b4879252ed5ce079a1
Coto <programmer.coto.1988@gmail.com>
2021-04-07 13:33:55 -0400

    update license

--- targetClientRenderers/NintendoDS/TexturedBlenderModelNDSRender/arm9/source/main.c
+++ targetClientRenderers/NintendoDS/TexturedBlenderModelNDSRender/arm9/source/main.c
@@ -17,6 +17,31 @@ USA
 
 */
 
+/*---------------------------------------------------------------------------------
+
+ 	Copyright (C) 2005
+		Jason Rogers (dovoto)
+		Dave Murphy (WinterMute)
+
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any
+	damages arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any
+	purpose, including commercial applications, and to alter it and
+	redistribute it freely, subject to the following restrictions:
+
+	1.	The origin of this software must not be misrepresented; you
+		must not claim that you wrote the original software. If you use
+		this software in a product, an acknowledgment in the product
+		documentation would be appreciated but is not required.
+	2.	Altered source versions must be plainly marked as such, and
+		must not be misrepresented as being the original software.
+	3.	This notice may not be removed or altered from any source
+		distribution.
+
+---------------------------------------------------------------------------------*/
+
 #include "main.h"
 #include "dsregs.h"
 #include "dsregs_asm.h"
@@ -178,8 +203,9 @@ char curChosenBrowseFile[MAX_TGDSFILENAME_LENGTH+1];
 void menuShow(){
 	clrscr();
 	printf("                              ");
-	printf(" Env Mapping example ");
+	printf(" TexturedBlenderModelNDSRender example ");
 	printf(" Use the D-PAD to move the rendered polygon ");
+	printf(" Polygon rendered and textured from Blender natively. ");
 	
 	printf("Available heap memory: %d", getMaxRam());
 	printf("Button (Select): this menu. ");
