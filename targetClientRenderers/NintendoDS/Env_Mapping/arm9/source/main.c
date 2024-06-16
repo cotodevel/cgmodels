5e7ec72627ee802a9392d9a35f930272102d1dd2
cotodevel <programmer.coto.1988@gmail.com>
2024-06-15 23:11:38 -0400

    update TGDS & TGDS Project to commit:
    https://bitbucket.org/Coto88/toolchaingenericds/commits/569901133a0a375f97be2e8e4c93928f9d81508b
    
    https://bitbucket.org/Coto88/toolchaingenericds-multiboot/commits/03a17a13fa39c12ce2211edc51d9a540a24345ce

--- targetClientRenderers/NintendoDS/Env_Mapping/arm9/source/main.c
+++ targetClientRenderers/NintendoDS/Env_Mapping/arm9/source/main.c
@@ -45,6 +45,11 @@ USA
 #include "videoTGDS.h"
 #include "Suzanne.h"
 #include "cafe.h"
+#include "loader.h"
+
+//ARM7 VRAM core
+#include "arm7vram.h"
+#include "arm7vram_twl.h"
 
 char curChosenBrowseFile[MAX_TGDSFILENAME_LENGTH+1];
 
@@ -80,6 +85,20 @@ __attribute__ ((optnone))
 int main(int argc, char **argv) {
 	
 	/*			TGDS 1.6 Standard ARM9 Init code start	*/
+	//Save Stage 1: IWRAM ARM7 payload: NTR/TWL (0x03800000)
+	memcpy((void *)TGDS_MB_V3_ARM7_STAGE1_ADDR, (const void *)0x02380000, (int)(96*1024));	//
+	coherent_user_range_by_size((uint32)TGDS_MB_V3_ARM7_STAGE1_ADDR, (int)(96*1024)); //		also for TWL binaries 
+	
+	//Execute Stage 2: VRAM ARM7 payload: NTR/TWL (0x06000000)
+	u32 * payload = NULL;
+	if(__dsimode == false){
+		payload = (u32*)&arm7vram[0];	
+	}
+	else{
+		payload = (u32*)&arm7vram_twl[0];
+	}
+	executeARM7Payload((u32)0x02380000, 96*1024, payload);
+	
 	bool isTGDSCustomConsole = true;	//set default console or custom console: true
 	GUI_init(isTGDSCustomConsole);
 	GUI_clear();
