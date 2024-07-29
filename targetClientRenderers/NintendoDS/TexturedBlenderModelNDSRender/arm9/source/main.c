099563d34f7de3b406190015b4465bea8392cd3f
cotodevel <programmer.coto.1988@gmail.com>
2024-07-28 22:28:26 -0400

    28 July 2024 bugfixes:
    -TWL mode homebrew is now guaranteed to have 15.7M~ worth of bootable TWL EWRAM
    
    -tgds-mb remoteboot working again on TWL hardware, now running properly through default TGDS-mb v3 arm7 bootloader
    
    -set up MBK (TWL WRAM) properly for TWL mode homebrew, fixes sound & improves compatibility on DKARM TWL mode homebrew.
    
    -merge detect NDS/TWL binary function
    
    -ARM payloads are written in order now (lowest to highest)
    
    -TGDS-multiboot goes back to loader in TWL mode
    
    -fixed:
    
    	-shutdownNDSHardware();
    
    	-resetNDSHardware();
    
    	on NTR/TWL hardware
    
    -
    
    Add support for Super Mario 64 DSi port (DKARM): (https://github.com/Hydr8gon/sm64/commit/7d50caa3856be22dd167f1bfa874f8e5f3ad2b0 booting on ToolchainGenericDS-multiboot (DSi mode)
    
    -
    
    fix sound issues in NTR/TWL hardware when launching DKARM homebrew
    
    -
    
    -updated ARGV protocol again.
    
    -fixed TVS support again in TGDS-multimediaplayer
    
    Coto: this is the last commit I do on ToolchainGenericDS SDK, i'll be working on game ports from now on ...

--- targetClientRenderers/NintendoDS/TexturedBlenderModelNDSRender/arm9/source/main.c
+++ targetClientRenderers/NintendoDS/TexturedBlenderModelNDSRender/arm9/source/main.c
@@ -55,6 +55,15 @@ USA
 #include "arm7vram.h"
 #include "arm7vram_twl.h"
 
+u32 * getTGDSMBV3ARM7Bootloader(){
+	if(__dsimode == false){
+		return (u32*)&arm7vram[0];	
+	}
+	else{
+		return (u32*)&arm7vram_twl[0];
+	}
+}
+
 float rotateX = 0.0;
 float rotateY = 0.0;
 float camDist = 0.3*4;
@@ -259,8 +268,8 @@ int main(int argc, char **argv)   {
 			strcpy(&thisArgv[0][0], curChosenBrowseFile);	//Arg0:	Chainload caller: TGDS-MB
 			strcpy(&thisArgv[1][0], thisTGDSProject);	//Arg1:	NDS Binary reloaded through ChainLoad
 			strcpy(&thisArgv[2][0], (char*)arg0);	//Arg2: NDS Binary reloaded through ChainLoad's ARG0
-			addARGV(newArgc, (char*)&thisArgv);				
-			if(TGDSMultibootRunNDSPayload(curChosenBrowseFile) == false){ //should never reach here, nor even return true. Should fail it returns false
+			u32 * payload = getTGDSMBV3ARM7Bootloader();
+			if(TGDSMultibootRunNDSPayload(curChosenBrowseFile, (u8*)payload, newArgc, (char*)&thisArgv) == false){ //should never reach here, nor even return true. Should fail it returns false
 				
 			}
 		}
